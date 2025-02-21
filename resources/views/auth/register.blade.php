29901603cb846171744a1f5ba60a639a2b4487c8
John Rua <Jrua89@gmail.com>
2025-02-21 13:04:06 -0500

    on submit keep old data form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
         <label for="name" class="sr-only">Name</label>
         <input type="text" name="name" id="name" placeholder="Your name" 
         class='bg-gray-100 border-2 w-full p-4 rounded-lg 
-        @error("name") border-red-500 @enderror' value="">
+        @error("name") border-red-500 @enderror' value="{{ old('name') }}">
         @error('name')
         <div class="text-red-500 mt-2 text-sm">
         {{ $message }}
@@ -23,7 +23,7 @@
         <div class="mb-4">
         <label for="username" class="sr-only">Username</label>
         <input type="text" name="username" id="username" placeholder="Your Username" 
-        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="{{ old('username') }}">
         @error('username')
         <div class="text-red-500 mt-2 text-sm">
         {{ $message }}
@@ -33,7 +33,7 @@
         <div class="mb-4">
         <label for="email" class="sr-only">Email</label>
         <input type="email" name="email" id="email" placeholder="Your Email" 
-        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="{{ old('email') }}">
         @error('email')
         <div class="text-red-500 mt-2 text-sm">
         {{ $message }}
@@ -43,7 +43,7 @@
         <div class="mb-4">
         <label for="password" class="sr-only">Password</label>
         <input type="password" name="password" id="password" placeholder="Choose a Password" 
-        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="{{ old('password') }}">
         @error('password')
         <div class="text-red-500 mt-2 text-sm">
         {{ $message }}
@@ -53,7 +53,7 @@
         <div class="mb-4">
         <label for="password_confirmation" class="sr-only">Password again</label>
         <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Reapeat your Password" 
-        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="{{ old('password_confirmation') }}">
         @error('password_confirmation')
         <div class="text-red-500 mt-2 text-sm">
         {{ $message }}
