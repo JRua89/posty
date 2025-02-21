1ac92548c19a36343be46f76f296bc5b02a874d3
John Rua <Jrua89@gmail.com>
2025-02-21 12:51:40 -0500

    added error handle for each input register

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,36 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require("@tailwindcss/postcss"),
     ]);
+
+
+  
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'anchor-blue': '#2563eb',
+            },
+        },
+        listStyleType: {
+            none: 'none',
+            disc: 'disc',
+            decimal: 'decimal',
+            square: 'square',
+            roman: 'upper-roman',
+        }
+    },
+
+    plugins: [require('@tailwindcss/postcss')],
+};
