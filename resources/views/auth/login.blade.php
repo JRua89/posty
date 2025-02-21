6d2040cebffc5b77eee8dbd8f082b13b95803fa4
John Rua <Jrua89@gmail.com>
2025-02-21 16:00:57 -0500

    set login and logout

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="flex justify-center">
+    <div class="w-4/12 bg-white p-6 rounded-lg">
+    <div class="mb-4">
+    Login
+    </div>
+
+    @if(session('status'))
+    <div class="bg-red-500 p-4 rounded-lg mb-6 text-center text-black">
+        {{ session('status') }}
+        </div>
+        @endif
+        
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+                
+        <div class="mb-4">
+        <label for="email" class="sr-only">Email</label>
+        <input type="email" name="email" id="email" placeholder="Your Email" 
+        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="{{ old('email') }}">
+        @error('email')
+        <div class="text-red-500 mt-2 text-sm">
+        {{ $message }}
+        </div>
+        @enderror
+        </div>
+        <div class="mb-4">
+        <label for="password" class="sr-only">Password</label>
+        <input type="password" name="password" id="password" placeholder="Choose a Password" 
+        class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="{{ old('password') }}">
+        @error('password')
+        <div class="text-red-500 mt-2 text-sm">
+        {{ $message }}
+        </div>
+        @enderror
+        </div>
+        
+        <div>
+            <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
+        </div>
+    </form>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,23 +7,24 @@
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
-        <li><a href="" class="p-3">Home</a></li>
-        <li><a href="" class="p-3">Dashboard</a></li>
+        <li><a href="/" class="p-3">Home</a></li>
+        <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
         <li><a href="" class="p-3">Post</a></li>
         </ul>
 
         <ul class="flex items-center">
-            @if(auth()->user())
+            @auth
             <li><a href="" class="p-3">{{ auth()->user()->name }}</a></li>
-            <li><a href="" class="p-3">Log Out</Out></a></li>
-            @else
-            <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
-            <li><a href="" class="p-3">Login</a></li>
-            @endif
+            <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                @csrf
+           <button type="submit">Log Out</Out></button>
+            </form>
+            @endauth
 
-    
-        
-       
+            @guest
+            <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            <li><a href="{{ route('login') }}" class="p-3">Login</a></li> 
+            @endguest
         
         </ul>
     </nav>
